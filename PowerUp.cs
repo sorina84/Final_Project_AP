668b326d1eb509c77e72eb4729def66247007844
sorina84 <afsharysorina@gmail.com>
2026-06-18 02:31:46 +0330

    Fixed some part of power up

--- PowerUp.cs
+++ PowerUp.cs
@@ -5,16 +5,19 @@ public class PowerUp : GameEntity
 {
     public enum PowerUpType
     {
-        Health,
-        SpeedBoost,
-        Shield
+        HealthPack,
+        FireRateBoost,
+        Shield,
+        TripleShot
     }
     public PowerUpType Type {get; private set;}
+    public float Radius {get;private set;} = 12f;
 
     public PowerUp(float x , float y , PowerUp type)
     :base(x ,y ,1.5f)
     {
         Type = type;
+        IsActive = true;
     }
 
     public override void Update()
@@ -28,7 +31,7 @@ public class PowerUp : GameEntity
             IsActive = false;
     }
 
-    public override void Draw(Graphic g)
+    public override void Draw(Graphics g)
     {
         if(!IsActive) 
             return;
@@ -38,12 +41,12 @@ public class PowerUp : GameEntity
         string T = Type.ToString();
         switch(T)
         {
-            case "Health":
+            case "HealthPack":
             {
                 brush = Brushes.LimeGreen;//*
                 break;
             }
-            case "SpeedBoost":
+            case "FireRateBoost":
             {
                 brush = Brushes.Gold; //*
                 break;
@@ -53,10 +56,14 @@ public class PowerUp : GameEntity
                 brush = Brushes.DodgerBlue; //*
                 break;
             }
+            case "TripleShot":
+            {
+                brush = Brushes.Orange;
+            }
             default:
                 brush = Brushes.White;
                 break;
         }
-        g.FillEllipse(brush , X -12 , Y -12 , 24 ,24);
+        g.FillEllipse(brush , X -Radius , Y -Radius , Radius*2 , Radius*2);
     }
 }

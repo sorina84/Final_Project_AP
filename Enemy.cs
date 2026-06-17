ee1c174e081f1753c1589559ed17814b9256fbdb
sorina84 <afsharysorina@gmail.com>
2026-06-17 22:03:15 +0330

    Update Enemy File for first time

--- Enemy.cs
+++ Enemy.cs
@@ -14,3 +14,28 @@ public abstract class Enemy : GameEntity
     }
 }
 
+public class BasicEnemy : Enemy
+{
+    public BasicEnemy(float x ,float y)
+    :base(x ,y ,2f , health = 30 , damage = 10 )
+    {
+    }
+
+    public override void Update()
+    {
+        if(!IsActive)
+            return;
+        
+        Y += speed ;
+        if(Y > 650)
+            IsActive =false;
+    }
+
+    public override void Draw(Graphic g)
+    {
+        if(!IsActive)
+            return;
+
+        g.FillRectangle(Brushes.Red , x -15 , y-15 , 30 , 30);
+    }
+}

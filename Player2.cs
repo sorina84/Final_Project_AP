310337a79596aff84fbc3545526e5ed82bc041c0
Negar <negar.ehsanipoor@gmail.com>
2026-06-24 07:06:35 +0330

    Add player 2 and gameobject for edit

---
+++ Player2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameObject
+{
+    using System.Drawing;
+
+    public class Player : GameObject
+    {
+        public int Lives { get; set; }
+        public int FireRate { get; set; }
+        public Player()
+        {
+            Hp = 100;
+            Lives = 3;
+            FireRate = 200;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+namespace GameObject
+{
+    public abstract class GameObject
+    {
+        //public float X { get; set; }
+        //public float Y { get; set; }
+        //public int Width { get; set; } = 40;
+        //public int Height { get; set; } = 40;
+        //public Rectangle Bounds => new Rectangle((int)X, (int)Y, Width, Height);
+        public Rectangle Bounds { get; set; }
+        public int Hp { get; set; }
+        public bool IsDestroyed => Hp <= 0;        
+    }
+}

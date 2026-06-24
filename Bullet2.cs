b95318260f65783439631640deedd43300621133
Negar <negar.ehsanipoor@gmail.com>
2026-06-24 06:56:18 +0330

    Add bullet 2 for edit

---
+++ Bullet2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace GameObject
+{
+    public class Bullet: GameObject
+    {
+        public int Damage { get; set; }
+        public bool IsPlayerBullet { get; set; }
+    }
+
+}

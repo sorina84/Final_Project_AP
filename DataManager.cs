23e0e31a9477d051c8a37a5909e6de9e96b4a1e2
Negar <negar.ehsanipoor@gmail.com>
2026-07-04 12:15:41 +0330

    Add ShopManager and DataManager

---
+++ DataManager.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace GameEntity
+{
+    public static class DataManager
+    {
+        private const string ConnectionString =
+            "Data Source=GameData.db;Version=3;";
+
+        //------------------------------------------
+
+        public static void InitializeDatabase()
+        {
+            using (SQLiteConnection connection =
+                new SQLiteConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string sql =
+                @"CREATE TABLE IF NOT EXISTS PlayerData
+                (
+                    Id INTEGER PRIMARY KEY,
+
+                    Coins INTEGER,
+
+                    HighScore INTEGER,
+
+                    ShipSkin TEXT,
+
+                    BulletSkin TEXT,
+
+                    BackgroundSkin TEXT,
+
+                    ExtraLives INTEGER
+                );";
+
+                SQLiteCommand command =
+                    new SQLiteCommand(sql, connection);
+
+                command.ExecuteNonQuery();
+
+                sql =
+                @"INSERT OR IGNORE INTO PlayerData
+                (Id,Coins,HighScore,ShipSkin,BulletSkin,BackgroundSkin,ExtraLives)
+
+                VALUES
+                (1,0,0,'Default','Default','Default',0);";
+
+                command =
+                    new SQLiteCommand(sql, connection);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        //------------------------------------------
+
+        public static void Save(PlayerData data)
+        {
+            using (SQLiteConnection connection =
+                new SQLiteConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string sql =
+                @"UPDATE PlayerData
+
+                SET
+
+                Coins=@Coins,
+
+                HighScore=@HighScore,
+
+                ShipSkin=@ShipSkin,
+
+                BulletSkin=@BulletSkin,
+
+                BackgroundSkin=@BackgroundSkin,
+
+                ExtraLives=@ExtraLives
+
+                WHERE Id=1;";
+
+                SQLiteCommand command =
+                    new SQLiteCommand(sql, connection);
+
+                command.Parameters.AddWithValue("@Coins",
+                    CoinManager.Coins);
+
+                command.Parameters.AddWithValue("@HighScore",
+                    ScoreManager.Score);
+
+                command.Parameters.AddWithValue("@ShipSkin",
+                    data.ShipSkin);
+
+                command.Parameters.AddWithValue("@BulletSkin",
+                    data.BulletSkin);
+
+                command.Parameters.AddWithValue("@BackgroundSkin",
+                    data.BackgroundSkin);
+
+                command.Parameters.AddWithValue("@ExtraLives",
+                    data.ExtraLives);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        //------------------------------------------
+
+        public static PlayerData Load()
+        {
+            PlayerData data = new PlayerData();
+
+            using (SQLiteConnection connection =
+                new SQLiteConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string sql =
+                    "SELECT * FROM PlayerData WHERE Id=1;";
+
+                SQLiteCommand command =
+                    new SQLiteCommand(sql, connection);
+
+                SQLiteDataReader reader =
+                    command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    CoinManager.SetCoins(
+                        Convert.ToInt32(reader["Coins"]));
+
+                    ScoreManager.SetScore(
+                        Convert.ToInt32(reader["HighScore"]));
+
+                    data.ShipSkin =
+                        reader["ShipSkin"].ToString();
+
+                    data.BulletSkin =
+                        reader["BulletSkin"].ToString();
+
+                    data.BackgroundSkin =
+                        reader["BackgroundSkin"].ToString();
+
+                    data.ExtraLives =
+                        Convert.ToInt32(reader["ExtraLives"]);
+                }
+            }
+
+            return data;
+        }
+
+        //------------------------------------------
+
+        public static void Reset()
+        {
+            CoinManager.Reset();
+
+            ScoreManager.Reset();
+
+            Save(new PlayerData());
+        }
+    }
+}
+
+
+}
---
+++ ShopManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEntity
+{
+    public enum ShopItem
+    {
+        FalconShip,
+        CyberShip,
+        GreenBullet,
+        PlasmaBullet,
+        GalaxyBackground,
+        MarsBackground,
+        ExtraLife
+    }
+    public class PlayerData
+    {
+        // اطلاعات ذخیره‌شونده
+        //public int Coins { get; set; }
+        //public int HighScore { get; set; }
+        // آیتم‌های خریداری شده
+        public string ShipSkin { get; set; }
+        public string BulletSkin { get; set; }
+        public string BackgroundSkin { get; set; }
+        // آیتم مصرفی
+        public int ExtraLives { get; set; }
+        public PlayerData()
+        {
+            //Coins = 0;
+            //HighScore = 0;
+            ShipSkin = "Default";
+            BulletSkin = "Default";
+            BackgroundSkin = "Default";
+            ExtraLives = 0;
+        }
+    }
+
+
+    public class ShopManager
+    {
+        private readonly Dictionary<ShopItem, int> prices;
+
+        public ShopManager()
+        {
+            prices = new Dictionary<ShopItem, int>()
+            {
+                { ShopItem.FalconShip, 500 },
+                { ShopItem.CyberShip, 800 },
+
+                { ShopItem.GreenBullet, 300 },
+                { ShopItem.PlasmaBullet, 600 },
+
+                { ShopItem.GalaxyBackground, 400 },
+                { ShopItem.MarsBackground, 700 },
+
+                { ShopItem.ExtraLife, 200 }
+            };
+        }
+
+        //--------------------------------------------------
+
+        public int GetPrice(ShopItem item)
+        {
+            return prices[item];
+        }
+
+        //--------------------------------------------------
+
+        public bool CanBuy(ShopItem item)
+        {
+            return CoinManager.Coins >= GetPrice(item);
+        }
+
+        //--------------------------------------------------
+
+        public bool BuyItem(PlayerData data, ShopItem item)
+        {
+            if (!CoinManager.SpendCoins(GetPrice(item)))
+                return false;
+
+            switch (item)
+            {
+                case ShopItem.FalconShip:
+                    data.ShipSkin = "Falcon";
+                    break;
+
+                case ShopItem.CyberShip:
+                    data.ShipSkin = "Cyber";
+                    break;
+
+                case ShopItem.GreenBullet:
+                    data.BulletSkin = "Green";
+                    break;
+
+                case ShopItem.PlasmaBullet:
+                    data.BulletSkin = "Plasma";
+                    break;
+
+                case ShopItem.GalaxyBackground:
+                    data.BackgroundSkin = "Galaxy";
+                    break;
+
+                case ShopItem.MarsBackground:
+                    data.BackgroundSkin = "Mars";
+                    break;
+
+                case ShopItem.ExtraLife:
+                    data.ExtraLives++;
+                    break;
+            }
+
+            // ذخیره تغییرات
+            DataManager.Save(data);
+
+            return true;
+        }
+    }
+}

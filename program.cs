ff6475f66a674314bf439d44c79f088b9fb37a00
Negar <negar.ehsanipoor@gmail.com>
2026-07-07 22:13:54 +0330

    Form -> GameForm

--- program.cs
+++ program.cs
@@ -8,6 +8,6 @@ static class Program
     {
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new Form1());
+        Application.Run(new GameForm());
     }
 }

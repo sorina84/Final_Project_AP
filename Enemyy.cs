717134669bc17e740c6f10a4062fcde97c2bac20
Negar <negar.ehsanipoor@gmail.com>
2026-07-07 21:21:23 +0330

    Add Property (Image Sprite,Width,Height) ,Edit Bounds (return), Add method (Draw)

--- Enemyy.cs
+++ Enemyy.cs
@@ -4,24 +4,41 @@ using System.Drawing;
 
 namespace GameObject
 {
-    public abstract class Enemy : GameObject
+    public abstract class Enemy : GameEntity
     {
+        public Image Sprite { get; protected set; }
+        public int Width { get; protected set; } = 50;
+        public int Height { get; protected set; } = 50;
+
         public int Hp { get; set; }
         public int ScoreValue { get; set; }
         public int CoinValue { get; set; }
-        public virtual int Width => 40;
-        public virtual int Height => 40;
+
 
         public RectangleF Bounds
         {
             get
             {
-                return new RectangleF(X - Width / 2f, Y - Height / 2f, Width, Height);
+                return new RectangleF(X, Y, Width, Height);
             }
         }
 
         protected Enemy(float x, float y, float speed) : base(x, y, speed)
         {
+        }
+
+        public override void Draw(Graphics g)
+        {
+            if (!IsActive)
+                return;
+            if (Sprite != null)
+            {
+                g.DrawImage(Sprite, X, Y, Width, Height);
+            }
+            else
+            {
+                g.FillRectangle(Brushes.Red, X, Y, Width, Height);
+            }
         }
 
         public virtual void Attack()

567ce9afa5471258bdc90dfa14fbfb358d0e1bb1
sorina84 <afsharysorina@gmail.com>
2026-07-08 15:08:30 +0330

    align namespaces and project references

--- GameEntity.cs
+++ GameEntity.cs
@@ -1,43 +1,47 @@
-using System;
 using System.Drawing;
 
-public abstract class GameEntity
+namespace GameEntity
 {
-    private float _x;
-    private float _y;
-    private float _speed;
-    private bool _isActive;
+    public abstract class GameEntity
+    {
+        private float _x;
+        private float _y;
+        private float _speed;
+        private bool _isActive;
 
-    public float X
-    {
-        get => _x;
-        set => _x = value;
-    }
+        public float X
+        {
+            get => _x;
+            set => _x = value;
+        }
 
-    public float Y
-    {
-        get => _y;
-        set => _y = value;
-    }
+        public float Y
+        {
+            get => _y;
+            set => _y = value;
+        }
 
-    public float Speed
-    {
-        get => _speed;
-        protected set => _speed = value;
-    }
+        public float Speed
+        {
+            get => _speed;
+            set => _speed = value;
+        }
 
-    public bool IsActive
-    {
-        get => _isActive;
-        set => _isActive = value;
+        public bool IsActive
+        {
+            get => _isActive;
+            set => _isActive = value;
+        }
+
+        protected GameEntity(float x, float y, float speed)
+        {
+            _x = x;
+            _y = y;
+            _speed = speed;
+            _isActive = true;
+        }
+
+        public abstract void Update(float deltaTime);
+        public abstract void Draw(Graphics g);
     }
-    protected GameEntity(float x, float y, float speed)
-    {
-        _x = x;
-        _y = y;
-        _speed = speed;
-        _isActive = true;
-    }
-    public abstract void Update(float deltaTime);
-    public abstract void Draw();
 }
